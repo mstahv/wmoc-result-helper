c0d568ab23a7f73ecc36a5fc9ab7e07cc2a2358d
Matti Tahvonen <matti@vaadin.com>
2023-12-27 23:15:06 +0200

    about view tuning

--- src/main/java/org/example/MainView.java
+++ src/main/java/org/example/MainView.java
@@ -2,16 +2,20 @@ package org.example;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import org.vaadin.firitin.appframework.MenuItem;
 import org.vaadin.firitin.components.RichText;
 
 @Route(layout = TopLayout.class)
+@MenuItem(title = "About", order = MenuItem.BEGINNING)
 public class MainView extends VerticalLayout {
 
     public MainView() {
         add(new RichText().withMarkDown("""
+        # WMOC result service helper
+        
         This app contains various helpers for WMOC result service.
         
-        Source code (Open Source): https://github.com/mstahv/wmoc-result-helper
+        [Source code available in GitHub (Open Source)](https://github.com/mstahv/wmoc-result-helper).
         
         """));
     }
