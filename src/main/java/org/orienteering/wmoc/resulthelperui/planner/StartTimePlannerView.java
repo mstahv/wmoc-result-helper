6fd4446d226510f2e0c25225ae6b5af170c7c59e
Matti Tahvonen <matti@vaadin.com>
2024-05-25 10:38:42 +0300

    Trivial validation for planner

--- src/main/java/org/orienteering/wmoc/resulthelperui/planner/StartTimePlannerView.java
+++ src/main/java/org/orienteering/wmoc/resulthelperui/planner/StartTimePlannerView.java
@@ -2,6 +2,8 @@ package org.orienteering.wmoc.resulthelperui.planner;
 
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.html.Pre;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -11,6 +13,7 @@ import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.orienteering.wmoc.domain.planner.AllPlans;
+import org.orienteering.wmoc.domain.planner.Clazz;
 import org.orienteering.wmoc.domain.planner.Start;
 import org.orienteering.wmoc.domain.planner.StartTimePlan;
 import org.orienteering.wmoc.resulthelperui.TopLayout;
@@ -24,7 +27,12 @@ import org.vaadin.firitin.util.VStyleUtil;
 import org.vaadin.firitin.util.style.Padding;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 @UIScope
 @Route(layout = TopLayout.class)
@@ -136,10 +144,11 @@ public class StartTimePlannerView extends VerticalLayout {
                         // rebuild view
                         edit(plan);
                     });
-
         });
 
-        add(new VHorizontalLayout(planName, remove, setAllIntervals));
+        add(new VHorizontalLayout(planName, remove, setAllIntervals, new VButton("Validate & statistics...", () -> {
+            showValidationAndStats();
+        })));
         binder.bindInstanceFields(this);
         binder.setBean(startTimePlan);
 
@@ -172,6 +181,56 @@ public class StartTimePlannerView extends VerticalLayout {
             planSelector.removeFromParent(); // in main layout
         }
         super.onDetach(detachEvent);
+    }
+
+    private void showValidationAndStats() {
+
+        StartTimePlan plan = plans.getValue();
+
+        int totalRunnersExpected = 0;
+        int classCount = 0;
+        Set<String> classNames = new TreeSet<>();
+        Set<String> dupes = new TreeSet<>();
+        List<Start> starts = plan.getStarts();
+        for(Start s : starts) {
+            Set<Clazz> startQueues = s.getStartQueues();
+            for(Clazz c : startQueues) {
+                while(c != null) {
+                    classCount++;
+                    if(classNames.contains(c.getName())) {
+                        dupes.add(c.getName());
+                    } else {
+                        classNames.add(c.getName());
+                    }
+                    totalRunnersExpected = totalRunnersExpected + c.getEstimatedRunners();
+                    c = c.getNextClazz();
+                }
+            }
+        }
+
+        String report = """
+            Total runners estimated/expected: %s
+        
+            Classes: %s 
+            %s
+            
+            Dupes:  %s (should be zero!) 
+            %s
+                
+            """.formatted(
+                totalRunnersExpected,
+                classCount, onePerLine(classNames),
+                dupes.size(), onePerLine(dupes)
+        );
+
+        Dialog dialog = new Dialog();
+        dialog.setHeaderTitle("Validation results:");
+        dialog.add(new Pre(report));
+        dialog.open();
+    }
+
+    private String onePerLine(Set<String> set) {
+        return set.stream().collect(Collectors.joining("\n"));
     }
 
 }
